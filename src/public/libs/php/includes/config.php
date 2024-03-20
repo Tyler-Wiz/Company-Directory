461bb7c7697f9832c82580313181b99283d935bc
tyler-Wiz <tylertooxclusive@gmail.com>
2024-03-20 17:04:04 +0000

    Fix:Remove single entry

--- src/public/libs/php/includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$servername = "db"; // Change this to your database server hostname
-$username = "root";     // Change this to your database username
-$password = "example";     // Change this to your database password
-$database = "companydirectory";     // Change this to your database name
